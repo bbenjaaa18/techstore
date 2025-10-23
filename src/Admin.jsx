9b89d1409007adb2843d52727f7385d8818580cb
bemellaa <be.mellaa@duocuc.cl>
2025-10-23 16:03:26 -0300

    Arreglo de errores react

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import "./Admin.css";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 
 export default function Admin() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
